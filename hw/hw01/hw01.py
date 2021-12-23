a6badccab10ae88875158641a8c8719d9620990e
suvix <suvisun33@gmail.com>
2021-12-23 10:51:37 -0500

    modify hailstone function

--- hw/hw01/hw01.py
+++ hw/hw01/hw01.py
@@ -128,9 +128,8 @@ def hailstone(n):
     while n > 1:
         if n % 2 == 0:
             n = n // 2
-            print(n)
         else:
             n = n * 3 + 1
-            print(n)
+        print(n)
         k += 1
     return k
